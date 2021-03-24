390395cde72e0de389f75653f76fda48e81deff8
Abner Hernandez <abneralberto-hernandezlopez@cunoc.edu.gt>
2021-03-24 02:44:14 -0600

    creacion de proyectos en nb e ij

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author abnerhl
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Primer ejecucion");
+        System.out.println("Segunda ejecucion");
+    }
+}
